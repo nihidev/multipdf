e94edee065a7e0e0425575020b47068307ba8b64
nihidev <nihilsdesk@gmail.com>
2023-11-06 23:22:24 +0100

    changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,38 @@ import os
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
+st.title("Reports Reader")
+
+data_directory = 'data/'
+
+def get_document_names(directory):
+    """ Get a list of document names in the specified directory """
+    # List all files in the directory
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+
+# Use Streamlit's sidebar to list document names with numbers
+st.sidebar.title('Document List')
+
+# Get list of documents
+documents = get_document_names(data_directory)
+
+# Use a sidebar for display with numbers
+for idx, document in enumerate(documents, start=1):
+    st.sidebar.text(f"{idx}. {document}")
+
+
+
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Ask me anything from the document list"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the documents"):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Finance and banking Regulation and your job is to answer technical questions. Assume that all questions are related to files uploaded. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
